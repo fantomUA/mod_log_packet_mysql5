429dd4ba28d6f124ca03c3d2008704149c5c7017
Taras Repa <trepa@malkosua.com>
2014-11-23 19:20:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+mod_log_chat_mysql
+============
+
+Developed by Jérôme Sautret <jerome.sautret@process-one.net>, Adapted for DB Logging by Michael Weibel <michael.weibel@amiadogroup.com>.
+
+Prerequisite
+============
+In order to compile Emysql, Erlang R13 or newer will need to be installed.
+If you are using the 2.1.10 ejabberd installer, that comes with an old version of Erlang and you will not be able to compile Emysql.
+
+Installation
+============
+  * Download Emysql: https://github.com/Eonblast/Emysql
+  * cd Emysql && make
+  * Make sure that emysql.app is present in the Emysql/ebin folder. If not build the Emysql module again.
+  * In another directory download ejabbed-modules from subversion: svn co https://svn.process-one.net/ejabberd-modules
+  * Copy Emysql/ebin/* to your ejabberd-modules (ebin) folder: cp Emysql/ebin/* ejabberd-modules/ejabberd-dev/trunk/ebin/
+  * Download mod_log_chat_mysql5 : git clone https://github.com/candy-chat/mod_log_chat_mysql5.git
+  * Move the mod_log_chat_mysql5 directory into the root of the ejabberd-modules folder
+  * Navigate to the mod_log_chat_mysql5 directory in the ejabberd-modules folder and call ./build.sh
+  * If successful the module has been compiled and output to ebin/mod_log_chat_mysql5.beam. Copy this file to your ejabberd system ebin folder folder (e.g. /usr/lib/ejabberd/ebin on Debian)
+  * Copy all the Emysql files to your ejabberd system ebin folder as well. (Emysql/ebin/*)
+  * Create required mysql table like this
+
+```sql
+  CREATE TABLE `mod_log_chat` ( 
+    `id` Int( 11 ) AUTO_INCREMENT NOT NULL, 
+    `fromJid` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+    `toJid` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+    `sentDate` Timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    `body` Text CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
+    `type` VarChar( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
+    `msg_id` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+     PRIMARY KEY ( `id` )
+  ) ENGINE = InnoDB CHARACTER SET = utf8;
+```
+  * See conf/ejabberd.conf.sample for an example configuration
+  * Once the ejabberd module is loaded and you have started ejabberd.  Look at the log files to see if the module has been correctly started (erlang.log) 
---
+++ src/mod_log_packet_mysql5.erl
@@ -0,0 +1,185 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_log_chat_mysql5.erl
+%%% Author  : Michael Weibel <michael.weibel@amiadogroup.com>
+%%% Original Author  : Jérôme Sautret <jerome.sautret@process-one.net>
+%%% Purpose : Log chat messages to a mysql db
+%%%----------------------------------------------------------------------
+
+-module(mod_log_packet_mysql5).
+-author('michael.weibel@amiadogroup.com').
+
+-behaviour(gen_mod).
+-behaviour(gen_server).
+
+%% gen_mod callbacks
+-export([start/2, start_link/2,
+		stop/1,
+		log_packet_send/3]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+		terminate/2, code_change/3]).
+
+%%-define(ejabberd_debug, true).
+
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
+-define(PROCNAME, ?MODULE).
+
+table_name() ->
+	"mod_log_packet".
+
+%% start db connection
+start_link(Host, Opts) ->
+	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	gen_server:start_link({local, Proc}, ?MODULE, [Host, Opts], []).
+
+%% Start module & start gen_server as a child for the db connection
+start(Host, Opts) ->
+	ejabberd_hooks:add(user_send_packet, Host, ?MODULE, log_packet_send, 55),
+	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+
+	ChildSpec =
+			{Proc,
+				{?MODULE, start_link, [Host, Opts]},
+				transient,
+				50,
+				worker,
+				[?MODULE]},
+	supervisor:start_child(ejabberd_sup, ChildSpec).
+
+%% stop module (remove hooks) & stop gen server
+stop(Host) ->
+	ejabberd_hooks:delete(user_send_packet, Host,
+		?MODULE, log_packet_send, 55),
+	Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+	gen_server:call(Proc, stop),
+	supervisor:delete_child(ejabberd_sup, Proc).
+
+%% called from start_link/2 and sets up the db connection
+init([_Host, Opts]) ->
+	?INFO_MSG("Starting ~p", [?MODULE]),
+
+	crypto:start(),
+	application:start(emysql),
+
+	Server = gen_mod:get_opt(server, Opts, "localhost"),
+	Port = gen_mod:get_opt(port, Opts, 3306),
+	DB = gen_mod:get_opt(db, Opts, "logdb"),
+	User = gen_mod:get_opt(user, Opts, "root"),
+	Password = gen_mod:get_opt(password, Opts, ""),
+	PoolSize = gen_mod:get_opt(pool_size, Opts, 1),
+	Encoding = gen_mod:get_opt(encoding, Opts, utf8),
+
+	?INFO_MSG("Opening mysql connection ~s@~s:~p/~s", [User, Server, Port, DB]),
+	emysql:add_pool(mod_log_chat_mysql5_db, PoolSize, User, Password, Server, Port, DB, Encoding),
+	{ok, undefined}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	?INFO_MSG("Terminate called", []),
+	emysql:remove_pool(mod_log_chat_mysql5_db),
+	emysql:stop(),
+	ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(stop, _From, State) ->
+	{stop, normal, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast({insert_packet, Id, Packet}, State) ->
+
+	?DEBUG("Save packet: ~s", [xml:element_to_string(Packet)]),
+	Query = ["INSERT INTO ", table_name(), " (msg_id, packet) VALUES", "(?, ?)"],
+	Xml = xml:element_to_string(Packet),
+	sql_query(Query, [Id, escape(text, Xml)]),
+	{noreply, State}.
+
+%% handle module infos
+handle_info({'DOWN', _MonitorRef, process, _Pid, _Info}, State) ->
+	{stop, connection_dropped, State};
+handle_info(Info, State) ->
+	?INFO_MSG("Got Info:~p, State:~p", [Info, State]),
+	{noreply, State}.
+
+%% ejabberd hook
+log_packet_send(From, To, Packet) ->
+	log_packet(From, To, Packet).
+
+log_packet(From, To, Packet = {xmlelement, "message", Attrs, _Els}) ->
+	case xml:get_attr_s("type", Attrs) of
+		"error" -> %% we don't log errors
+			?DEBUG("dropping error: ~s", [xml:element_to_string(Packet)]),
+			ok;
+		_ ->
+			write_packet(From, To, Packet, xml:get_attr_s("type", Attrs), xml:get_attr_s("id", Attrs))
+	end;
+log_packet(_From, _To, _Packet) ->
+	ok.
+
+%% parse message and send to db connection gen_server
+write_packet(From, _To, Packet, _Type, Id) ->
+	Body = escape(html, xml:get_path_s(Packet, [{elem, "body"}, cdata])),
+	case Body of
+		"" -> %% don't log empty messages
+			?DEBUG("not logging empty message from ~s",[jlib:jid_to_string(From)]),
+			ok;
+		_ ->
+			Proc = gen_mod:get_module_proc(From#jid.server, ?PROCNAME),
+			gen_server:cast(Proc, {insert_packet, Id, Packet})
+	end.
+
+%% ==================
+%% SQL Query API
+%% ==================
+
+escape(text, Text) ->
+	Text;
+escape(_, "") ->
+	"";
+escape(html, [$< | Text]) ->
+	"&lt;" ++ escape(html, Text);
+escape(html, [$& | Text]) ->
+	"&amp;" ++ escape(html, Text);
+escape(html, [Char | Text]) ->
+	[Char | escape(html, Text)].
+
+sql_query(Query, Params) ->
+	case sql_query_internal_silent(Query, Params) of
+		{error, Reason} ->
+			?INFO_MSG("~p while ~p", [Reason, lists:append(Query)]),
+			{error, Reason};
+		Rez -> Rez
+	end.
+
+sql_query_internal_silent(Query, Params) ->
+	?DEBUG("Query sql_query_internal_silent ~s", [Query]),
+	emysql:prepare(mod_log_chat_mysql5_stmt, Query),
+	emysql:execute(mod_log_chat_mysql5_db, mod_log_chat_mysql5_stmt, Params).
